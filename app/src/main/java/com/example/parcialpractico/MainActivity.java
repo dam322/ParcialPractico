799498f932246b80851cfd3479ddfc8cbbd5f693
Eduardo Bravo <yahir.eduardo.bravo@gmail.com>
2022-05-25 15:02:25 -0500

    Cambiar algunas distribuciones de los botones para la actividad principal, reparar el login y agregar login con Google

--- app/src/main/java/com/example/parcialpractico/MainActivity.java
+++ app/src/main/java/com/example/parcialpractico/MainActivity.java
@@ -1,7 +1,6 @@
 package com.example.parcialpractico;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -10,13 +9,22 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonArrayRequest;
-import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.Task;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -29,6 +37,10 @@ public class MainActivity extends AppCompatActivity {
     Boolean beLogin = false;
     RequestQueue requestQueue;
     private static final String URL1 = "https://628963f9e5e5a9ad3218cb51.mockapi.io/api/v1/users";
+    // google
+    GoogleSignInClient mGoogleSignInClient;
+    int RC_SIGN_IN = 0;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,22 +49,78 @@ public class MainActivity extends AppCompatActivity {
         btnLogin = findViewById(R.id.btnLogin);
         textUser = findViewById(R.id.textUser);
         textPassword = findViewById(R.id.textPassword);
-        tryText = findViewById(R.id.textView4);
+        tryText = findViewById(R.id.errorText);
         requestQueue = Volley.newRequestQueue(this);
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(getApplicationContext(), textUser.getText().toString(), Toast.LENGTH_SHORT).show();
+                //Toast.makeText(getApplicationContext(), textUser.getText().toString(), Toast.LENGTH_SHORT).show();
                 ArrayRequest();
-                if(beLogin){
-                    Intent ir = new Intent(getBaseContext(), Recursos.class);
-                    ir.addFlags(ir.FLAG_ACTIVITY_CLEAR_TASK | ir.FLAG_ACTIVITY_CLEAR_TOP);
-                    startActivity(ir);
-                }
+
+                // moví esta linea para que se ejecute cundo se genera el request, porque es un
+                // hilo, por ello en el momento de evaluar la bandera belogin
+//                if(beLogin){
+//                    Intent ir = new Intent(getBaseContext(), Recursos.class);
+//                    ir.addFlags(ir.FLAG_ACTIVITY_CLEAR_TASK | ir.FLAG_ACTIVITY_CLEAR_TOP);
+//                    startActivity(ir);
+//                }
 
             }
         });
+        // google
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+        findViewById(R.id.sign_in_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                signIn();
+            }
+        });
 
+
+    }
+
+//    @Override
+//    protected void onStart() {
+//        super.onStart();
+//        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
+//        //updateUI(account);
+//
+//    }
+
+    // try use google siging
+    private void signIn() {
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+
+        //startActivityForResult(signInIntent, RC_SIGN_IN); // deprecated
+        signInActivityResultLauncher.launch(signInIntent);
+
+    }
+    private ActivityResultLauncher<Intent> signInActivityResultLauncher = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            result -> {
+                // -1 account signin
+                // 0 no accoint
+                // 1 account
+
+                if (result.getResultCode() ==  -1 || result.getResultCode() == 1){
+                    Intent data = result.getData();
+                    Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+                    handleSignInResult(task);
+                }
+            }
+    );
+    private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
+        try {
+            GoogleSignInAccount account = completedTask.getResult(ApiException.class);
+            // Signed in successfully, show authenticated UI.
+            updateUI(account.getDisplayName());
+            //updateUI(account);
+        } catch (ApiException e) {
+            tryText.setText("Lo sentimos, ha ocurrido un error :C\nIntenta más tarde!");
+        }
     }
 
     private void ArrayRequest(){
@@ -61,19 +129,21 @@ public class MainActivity extends AppCompatActivity {
                 URL1,
                 null,
                 new Response.Listener<JSONArray>() {
+                    @SuppressLint("SetTextI18n")
                     @Override
                     public void onResponse(JSONArray response) {
+                        String name = textUser.getText().toString();
                         for(int i = 0; i<response.length(); i++){
                             try {
                                 JSONObject jsonObject = new JSONObject(response.get(i).toString());
                                 if(textUser.getText().toString().equals(jsonObject.getString("name")) && textPassword.getText().toString().equals(jsonObject.getString("password"))){
-                                    tryText.setText("Iniciar sesion ok!");
+                                    //tryText.setText("Iniciar sesion ok!");
                                     beLogin = true;
                                     textPassword.setText("");
                                     textUser.setText("");
                                     break;
                                 }else{
-                                    tryText.setText("crendenciales no coinciden");
+                                    tryText.setText("Credenciales incorrectas, por favor revise.");
                                     beLogin = false;
                                     textPassword.setText("");
                                     textUser.setText("");
@@ -81,6 +151,9 @@ public class MainActivity extends AppCompatActivity {
                             } catch (JSONException e) {
                                 e.printStackTrace();
                             }
+                        }
+                        if(beLogin){
+                            updateUI(name);
                         }
                     }
                 },
@@ -92,5 +165,12 @@ public class MainActivity extends AppCompatActivity {
                 }
         );
         requestQueue.add(arrayRequest);
+    }
+
+    private void updateUI(String name){
+        Toast.makeText(getApplicationContext(), "Hola " + name +"!! XD", Toast.LENGTH_SHORT).show();
+        Intent ir = new Intent(getBaseContext(), Recursos.class);
+        ir.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        startActivity(ir);
     }
 }
